14f66f24f5d97bb340802bc9001762b9704dc5c3
Mukilan <38306163+mukilan88@users.noreply.github.com>
2019-10-03 09:19:27 +0530

    Update settings.py

--- youtube/settings.py
+++ youtube/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!h!9-0(xu%#q@2a^q_3clhu97n^!&44r)(g49zy*ur2$wky^ye'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
